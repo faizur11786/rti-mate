799a99583f8bc0585cca86366628252e063debcf
Faizur Rahman <faizur11786@gmail.com>
2022-02-27 20:53:54 +0530

    Layout Component added

---
+++ components/Layout/Footer/index.jsx
@@ -0,0 +1,11 @@
+
+import styles from "./Footer.module.css"
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>This is Footer</p>
+        </footer>
+    )
+}
+
+export default Footer
